bb69a65b5c0edc55d95c8a6227a966d7c2d68137
luopeng <373450847@qq.com>
2021-08-09 11:19:35 +0800

    VUE开始

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <home></home>
   </div>
 </template>
+<script>
+import HelloWorld from "../components/HelloWorld";
+import  home from "./Home"
+export default {
+  name: "About",
+  components: {
+    home
+  },
+};
+</script>
